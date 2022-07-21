f394a01fd01189d61c13f2ce25747945ee1d7f47
iiap <1471127927@qq.com>
2022-07-21 15:31:47 +0800

    Live2D implemented.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <div id="app">
     <router-view/>
+    <div class="live2d-container">
+      <live2d
+        :style="style"
+        :model="['Potion-Maker/Pio', 'sorceress-costume']"
+        :direction="direction"
+        :size="size"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import live2d from 'vue-live2d'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    live2d
+  },
+  data() {
+    return {
+      direction: 'right',
+      style: '',
+      width: 500,
+      height: 500,
+      size: 350
+    }
+  }
+
 }
 </script>
+
+<style>
+.live2d-container {
+  opacity: 0;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+.live2d-container:hover {
+  opacity: 1;
+}
+</style>
